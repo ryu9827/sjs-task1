7f53bcea6d16f1ec8aafddc3874706505e6a39a5
Bruce Li <ryu9827@gmail.com>
2017-11-19 17:01:05 +1300

    add bootstrap css to img style; revise boat details fields;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,23 @@ import { BoatService } from "./boat.service";
 @Component({
     selector: 'app-root',
     template: `
+  <div class="container">
     <h1>{{title}}</h1>
-<h2>My Boats</h2>
-<ul class="boats">
-<li *ngFor="let boat of boats"
-    [class.selected]="boat === selectedBoat"
-(click)="onSelect(boat)">
-<span class="badge">{{boat.id}}</span> {{boat.name}}
-</li>
-</ul>
-<boat-detail [boat]="selectedBoat"></boat-detail>
+    <div *ngFor="let boat of boats">
+      <div class="card" [class.selected]="boat === selectedBoat" (click)="onSelect(boat)" style="width: 15rem;cursor: pointer">
+        <img class="card-img-top" src="{{boat.photo}}" alt="Card image cap" >
+        <div class="card-block">
+          <p class="card-title text-center">{{boat.name}}</p>          
+        </div>
+      </div>      
+    </div>
+    <boat-detail [boat]="selectedBoat"></boat-detail>
+  </div>
     `,
     providers:[BoatService]
 })
 export class AppComponent implements OnInit{
-    title = 'Dockyards';
+    title = 'Dockyards Boat Repair';
     boats : Boat[];
     selectedBoat: Boat;
 
--- src/app/boat-detail.component.ts
+++ src/app/boat-detail.component.ts
@@ -12,6 +12,35 @@ import {Boat} from "./boat";
           <label>name: </label>
           <input [(ngModel)]="boat.name" placeholder="name"/>
         </div>
+        <div>
+          <label>type: </label>
+          <input [(ngModel)]="boat.type" placeholder="type"/>
+        </div>
+        <div>
+          <label>photo: </label>
+          <input [(ngModel)]="boat.photo" placeholder="photo"/>
+        </div>
+        <div>
+          <label>length: </label>
+          <input [(ngModel)]="boat.length" placeholder="length"/>
+        </div>
+        <div>
+          <label>description: </label>
+          <input [(ngModel)]="boat.work_description" placeholder="description"/>
+        </div>
+        <div>
+          <label>arrival date: </label>
+          <input [(ngModel)]="boat.arrival_date" placeholder="arrival date"/>
+        </div>
+        <div>
+          <label>delivery date: </label>
+          <input [(ngModel)]="boat.delivery_date" placeholder="delivery date"/>
+        </div>
+        <div>
+          <label>status: </label>
+          <input [(ngModel)]="boat.status" placeholder="status"/>
+        </div>
+        <button class="btn btn-primary">save</button>
       </div>
     `
 })
